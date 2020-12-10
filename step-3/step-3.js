a1e3f25572f06b3b4705af5e24cd35fdf9928b1b
SEONKYU\difke <somedaycode@gmail.com>
2020-12-10 19:26:50 +0900

    각 함수마다 주석 추가

--- step-3/step-3.js
+++ step-3/step-3.js
@@ -1,5 +1,4 @@
-const cube = {};
-
+// 화면에 큐브를 출력
 function displayCube() {
   let count = 0;
   for (let i = 0; i < 6; i++) {
@@ -13,12 +12,14 @@ function displayCube() {
   }
 }
 
+// 3개의 row를 만듬
 function makeCube() {
   return Array(3)
-    .fill('')
+    .fill()
     .map(() => Array());
 }
 
+// 각각의 스타일 값을 3*3 배열에 집어넣음
 function putValues(number) {
   const cubeMaker = makeCube();
   let count = 0;
@@ -32,10 +33,13 @@ function putValues(number) {
 }
 
 function main() {
+  const cube = {};
   displayCube();
   for (let i = 1; i < 7; i++) {
+    // 총 6개의 cube.color를 생성
     cube[`color${i}`] = putValues(i);
   }
+  console.log(cube);
 }
 
 main();
