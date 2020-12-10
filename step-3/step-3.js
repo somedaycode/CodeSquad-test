aab7535f2e72cb8f43fb87ec07cd897c754a69cd
SEONKYU\difke <somedaycode@gmail.com>
2020-12-10 20:21:01 +0900

    큐브 앞면이 오른쪽으로 돌아가는 함수 작성중

--- step-3/step-3.js
+++ step-3/step-3.js
@@ -4,6 +4,12 @@ const cube = {
   movement: [],
 };
 
+/*
+
+초기 화면 구성 함수
+
+*/
+
 // 화면에 큐브를 출력
 function displayCube() {
   let count = 0;
@@ -38,14 +44,18 @@ function putValues(number) {
   return cubeMaker;
 }
 
-function startBtnHandler(e) {
-  const inputText = document.querySelector('.input-text');
-  const inputOrder = document.querySelector('.input-order');
-  inputOrder.textContent = inputText.value;
-  cube.movement.push(inputText.value);
-  inputText.value = '';
-  console.log(cube.movement);
-  moveCube();
+/*
+
+큐브 명령어 및 동작 함수 
+
+ */
+
+function frontClockwise(move) {
+  if (move !== 'F') return;
+
+  cube.color4.map((v) => {
+    v[0].className = 'color1';
+  });
 }
 
 // 작은따옴표 문자 검사
@@ -71,24 +81,40 @@ function quit(move) {
 // 큐브 동작
 function moveCube() {
   const moveOrder = cube.movement.join('').split('');
-  console.log(moveOrder);
   const finalMove = checkApostrophe(moveOrder);
   finalMove.forEach((move) => {
     if (move === 'Q') return quit(move);
     else {
+      frontClockwise(move);
       console.log('test');
     }
   });
   cube.movement = [];
 }
 
+/*
+
+  버튼 함수
+
+*/
+
+//실행 버튼
+function startBtnHandler(e) {
+  const inputText = document.querySelector('.input-text');
+  const inputOrder = document.querySelector('.input-order');
+  inputOrder.textContent = inputText.value;
+  cube.movement.push(inputText.value);
+  inputText.value = '';
+  moveCube();
+}
+
+// 메인 함수
 function main() {
   displayCube();
   for (let i = 1; i < 7; i++) {
     // 총 6개의 cube.color를 생성
     cube[`color${i}`] = putValues(i);
   }
-
   startBtn.addEventListener('click', startBtnHandler);
   console.log(cube);
 }
